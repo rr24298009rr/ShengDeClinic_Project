037a21c25bae23930877762d981a30e824ad4e34
rr24298009rr <rr24298009rr@gmail.com>
2023-07-15 01:37:33 +0800

    大致上的架構訂好了

--- MVC_Project/Controllers/HomeController.cs
+++ MVC_Project/Controllers/HomeController.cs
@@ -7,18 +7,26 @@ namespace MVC_Project.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
+            _logger = logger;           
         }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult ClinicIntroduction()
+        {
+            return View();
+        }
+        public IActionResult PhysicianIntroduction()
+        {
+            return View();
+        }
+
+        public IActionResult contact()
         {
             return View();
         }
--- MVC_Project/Program.cs
+++ MVC_Project/Program.cs
@@ -1,3 +1,6 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
