141823e1068a746f4d4cbbe0780ca5cfd32eef0d
2300709 <roywang@xsolutions.com.tw>
2025-07-16 23:36:10 +0800

    refactor : 換軟體架構

--- MVC_Project/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MVC_Project.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
